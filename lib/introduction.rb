8aacfd53716b7ae3fdb0f01b3436202b9f2e42bf
Jess Rezac <jess.rezac@gmail.com>
2019-04-11 03:39:10 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,5 @@
-def introduction(name)
-  puts "Hi, my name is #{name}."
-end
-
-def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+def stylish_chef
+  best_hairstyle = "Guy Fieri"
+  return "Martha Stewart"
+  "Guy Fieri"
 end
